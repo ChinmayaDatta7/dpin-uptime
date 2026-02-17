bc0820c5907264e1b6c8065715b519da7dd57cd4
ChinmayaDatta7 <lsrschinmayab@gmail.com>
2026-02-17 21:44:37 +0530

    refactor: enhance API error handling and improve response messages
    - Improved error handling in API endpoints for better clarity.
    - Updated response messages to provide more informative feedback to users.
    - Streamlined error logging for easier debugging.

---
+++ packages/db/seed.ts
@@ -0,0 +1,61 @@
+import { prismaClient } from "./src";
+
+const USER_ID = "4";
+
+async function seed() {
+  await prismaClient.user.create({
+    data: {
+      id: "1",
+      email: "test@test.com",
+    },
+  });
+
+  const website = await prismaClient.website.create({
+    data: {
+      id: "1",
+      url: "https://test.com",
+      userId: "1",
+    },
+  });
+
+  //   const validator = await prismaClient.validator.create({
+  //     data: {
+  //       publicKey: "0x12341223123",
+  //       location: "Delhi",
+  //       ip: "127.0.0.1",
+  //     },
+  //   });
+
+  await prismaClient.websiteTick.create({
+    data: {
+      id: "1",
+      websiteId: "1",
+      status: "Good",
+      createdAt: new Date(),
+      latency: 100,
+      validatorId: "1",
+    },
+  });
+
+  await prismaClient.websiteTick.create({
+    data: {
+      websiteId: website.id,
+      status: "Good",
+      createdAt: new Date(Date.now() - 1000 * 60 * 10),
+      latency: 100,
+      validatorId: "1",
+    },
+  });
+
+  await prismaClient.websiteTick.create({
+    data: {
+      websiteId: website.id,
+      status: "Bad",
+      createdAt: new Date(Date.now() - 1000 * 60 * 20),
+      latency: 100,
+      validatorId: "1",
+    },
+  });
+}
+
+seed();
