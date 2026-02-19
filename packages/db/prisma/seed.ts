1ab52be1ab0c12251a808481822268743cd4a77f
ChinmayaDatta7 <lsrschinmayab@gmail.com>
2026-02-19 21:04:30 +0530

    fix(db): enhance seed script with additional website and validator entries
    - Updated the seed function to create two websites and two validators dynamically.
    - Adjusted websiteTick entries to reference the newly created website and validator IDs.
    - Improved data diversity in the seed data for better testing scenarios.

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -10,7 +10,7 @@ async function seed() {
     },
   });
 
-  const website = await prismaClient.website.create({
+  const website1 = await prismaClient.website.create({
     data: {
       id: "2",
       url: "https://test.com",
@@ -18,7 +18,15 @@ async function seed() {
     },
   });
 
-  const validator = await prismaClient.validator.create({
+  const website2 = await prismaClient.website.create({
+    data: {
+      id: "3",
+      url: "https://test2.com",
+      userId: user.id,
+    },
+  });
+
+  const validator1 = await prismaClient.validator.create({
     data: {
       publicKey: "0x12341223123",
       location: "Delhi",
@@ -26,33 +34,61 @@ async function seed() {
     },
   });
 
+  const validator2 = await prismaClient.validator.create({
+    data: {
+      publicKey: "0x12341223121",
+      location: "Hyderabad",
+      ip: "127.0.0.2",
+    },
+  });
+
   await prismaClient.websiteTick.create({
     data: {
-      websiteId: website.id,
+      websiteId: website1.id,
       status: "Good",
       createdAt: new Date(),
       latency: 100,
-      validatorId: validator.id,
+      validatorId: validator1.id,
     },
   });
 
   await prismaClient.websiteTick.create({
     data: {
-      websiteId: website.id,
-      status: "Good",
-      createdAt: new Date(Date.now() - 1000 * 60 * 10),
-      latency: 100,
-      validatorId: validator.id,
-    },
-  });
-
-  await prismaClient.websiteTick.create({
-    data: {
-      websiteId: website.id,
+      websiteId: website2.id,
       status: "Bad",
       createdAt: new Date(Date.now() - 1000 * 60 * 20),
       latency: 100,
-      validatorId: validator.id,
+      validatorId: validator1.id,
+    },
+  });
+
+  await prismaClient.websiteTick.create({
+    data: {
+      websiteId: website2.id,
+      status: "Good",
+      createdAt: new Date(Date.now() - 1000 * 60 * 10),
+      latency: 100,
+      validatorId: validator1.id,
+    },
+  });
+
+  await prismaClient.websiteTick.create({
+    data: {
+      websiteId: website1.id,
+      status: "Bad",
+      createdAt: new Date(Date.now() - 1000 * 60 * 20),
+      latency: 100,
+      validatorId: validator2.id,
+    },
+  });
+
+  await prismaClient.websiteTick.create({
+    data: {
+      websiteId: website1.id,
+      status: "Good",
+      createdAt: new Date(Date.now() - 1000 * 60 * 20),
+      latency: 100,
+      validatorId: validator2.id,
     },
   });
 }
